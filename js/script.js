976479be3ec8017c770c7c46198cfb303104663e
hacor <hacornelis@gmail.com>
2016-08-13 13:57:35 +0200

    statische elementen

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ window.onload = function() {
 
         //Stap 04
 
-        banner.bind('KeyDown', function (e){
+        banner.bind('KeyDown', function (e) {
             if (e.key == Crafty.keys.RIGHT_ARROW) {
                 Crafty.enterScene("GameScene");
             }
@@ -20,11 +20,18 @@ window.onload = function() {
     });
 
     //Gamescene
-    Crafty.scene("GameScene", function() {
+    Crafty.scene("GameScene", function () {
         Crafty.background("#F0F");
         //Gravity X, Gravity Y, Pixel to Meter, speed up enabled
         Crafty.box2D.init(0, 10, 32, true);
-    } );
+    });
+    var wall1 = crafty.e("2D, Canvas,  Box2D, Color");
+    wall1.color("green").attr({x: 13, y: 100, w: 220, h: 12});
+    wall1.box2d({bodyType: 'static'});//Must be after positioning
+    var wall2 = crafty.e("2D, Canvas,Box2D, color");
+    wall2.color("green").attr({x: 140, y: 200, w: 220, h: 12});
+    wall2.box2d({bodyType: 'static'});
+});
 
     //Over naar stap 6!!
 
